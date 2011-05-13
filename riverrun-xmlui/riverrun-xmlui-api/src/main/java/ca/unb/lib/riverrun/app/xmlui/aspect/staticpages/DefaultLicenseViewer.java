581644df4936458fe28489c965d1aa618a0ba1e6
Jen Whitney <jen.j.whitney@gmail.com>
2011-05-13 10:40:56 -0300

    Bug 42 - fixed inconsistencies in 'license' vs 'licence'

--- riverrun-xmlui/riverrun-xmlui-api/src/main/java/ca/unb/lib/riverrun/app/xmlui/aspect/staticpages/DefaultLicenseViewer.java
+++ riverrun-xmlui/riverrun-xmlui-api/src/main/java/ca/unb/lib/riverrun/app/xmlui/aspect/staticpages/DefaultLicenseViewer.java
@@ -60,6 +60,9 @@ public class DefaultLicenseViewer extends AbstractDSpaceTransformer implements C
     @Override
     public void addBody(Body body) throws SAXException, WingException,
             UIException, SQLException, IOException, AuthorizeException {
+        
+        //
+
 
         // Explain a bit about the license
         Division preamble = body.addDivision("default-license");
